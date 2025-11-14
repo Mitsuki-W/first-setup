f0979d4248daa328239eb53ce77cb963475a2809
Mitsuki W <eng.m.watanabe.99@gmail.com>
2025-11-14 15:08:42 -0500

    fix: JSONファイルのフォーマットを整理し、productsスキーマのインデントを修正

--- db/schemas/products.ts
+++ db/schemas/products.ts
@@ -2,14 +2,14 @@ import { integer, pgTable, text, timestamp } from "drizzle-orm/pg-core";
 import { nanoid } from "nanoid";
 
 export const products = pgTable("products", {
-	id: text("id")
-		.primaryKey()
-		.$defaultFn(() => nanoid()),
-	name: text("name").notNull(),
-	price: integer("price").notNull(),
-	createdAt: timestamp("created_at").defaultNow().notNull(),
-	updatedAt: timestamp("updated_at")
-		.defaultNow()
-		.$onUpdate(() => new Date())
-		.notNull(),
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => nanoid()),
+  name: text("name").notNull(),
+  price: integer("price").notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at")
+    .defaultNow()
+    .$onUpdate(() => new Date())
+    .notNull(),
 });
